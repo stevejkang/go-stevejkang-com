274c01e35d0fd8169df367b5acca9416677231f4
stevejkang <iam@juneyoung.io>
2026-02-17 00:10:32 +0900

    Remove unnecessary React import and simplify Fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import React from 'react';
 import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <>
       <Component {...pageProps} />
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
       <noscript>
-        {/* eslint-disable @next/next/no-img-element */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="https://queue.simpleanalyticscdn.com/noscript.gif"
           alt=""
           referrerPolicy="no-referrer-when-downgrade"
         />
       </noscript>
-    </React.Fragment>
+    </>
   );
 }
 
